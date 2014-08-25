bcab0cb721db231ccf7d86bb542dffd69a348711
jazzcoder <jesse.eikema1991@gmail.com>
2014-08-25 18:01:30 +0200

    working scripts + hori nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,12 @@ var value = 0;
 $('.nav__button').click(function(){
 	switch (value) {
 		case 0 :
-			$('.nav__list').show();
+			$('.nav--off-window').show();
 			value = 1;
 			console.log('show');
 			break;
 		case 1 :
-			$('.nav__list').hide();
+			$('.nav--off-window').hide();
 			value = 0;
 			console.log('hide');
 			break;
