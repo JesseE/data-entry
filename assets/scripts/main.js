95b7243ebcabfa68b37f23caa7348f4f97d46957
jazzcoder <jesse.eikema1991@gmail.com>
2014-08-25 19:55:18 +0200

    horinav working like melkweg weg nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,15 +3,13 @@ $('.nav__button').click(function(){
 	switch (value) {
 		case 0 :
 			$('.nav--off-window').show();
-			$('header').addClass('menu-active');
-			$('section').addClass('menu-active');
+			$('header, section, footer').addClass('menu-active');
 			value = 1;
 			console.log('show');
 			break;
 		case 1 :
 			$('.nav--off-window').hide();
-			$('header').removeClass('menu-active');
-			$('section').removeClass('menu-active');
+			$('header, section, footer').removeClass('menu-active');
 			value = 0;
 			console.log('hide');
 			break;
