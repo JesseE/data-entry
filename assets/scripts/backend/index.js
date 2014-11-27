b8c431f4e5a98a11b546e79f801346f5407df081
jazzcoder <jesse.eikema1991@gmail.com>
2014-11-27 19:18:24 +0100

    templating optimsation

--- assets/scripts/backend/index.js
+++ assets/scripts/backend/index.js
@@ -77,9 +77,7 @@ function getAllMessage () {
         }).headers(header).end(function(response){  
            // var object = {"comment" : response.body.commit.message};     
             var object = response.body.commit.message;
-            console.log('first : '+ object);
             object = object.replace(/(\r\n|\n|\r)/gm,"");
-            console.log('second : '+ object);
             gitMessage.push(object); 
             completeReq();  
         });
@@ -107,36 +105,109 @@ module.exports.create = function(request, response){
         JSON.parse("[\"bucket[i]\"]");  
         container.push(bucket);
     };
-    var imgEntry = [
-        {"img":"../assets/images/modularv1.png"},
-        {"img":"../assets/images/modularv4.png"},
-        {"img":"../assets/images/modularv2.png"}
-    ];
-    var titleEntry = [
-        {"title": "Datavisualisatie"},
-        {"title": "Resizer"},
-        {"title": "Melkweg"},
-        {"title": "Score app"},
-        {"title": "Klassiekwijzer"},
-        {"title": "Pathogen"},
-    ];
-    var paragraphEntry =[
-        {"paragraph": "visualisatie van coding activiteit"},
-        {"paragraph": "Pas de dimensies van je afbeelding aan"},
-        {"paragraph": "SASS aanpassingen in de melkweg"},
-        {"paragraph": "Toevoegen van actuele score's"},
-        {"paragraph": "Kom erachter welke klassieke muziek bij je past"},
-        {"paragraph": "RTS game gemaakt met javascript"},
-    ];
-    var textEntry = [
-        {"text":"Als Frontend Developer is het mijn taak om ervoor te zorgen dat interactie tussen de gebruiker en digitale interactieve producten vloeiend en gebruiksvriendelijk gebeurt."},
-        {"text":"Tijdens het programeren splits ik mijn code afhankelijk van zijn functies op in aparte modules, zodat het achteraf makkelijk te onderhouden is"},
-        {"text":"Naast dat aparte modules een duidelijk overzicht maakt van de geschreven code, is het ook eenvoudig om het in een andere project toe te voegen. Dit maakt de code een stuk beter herbruikbaar. Je hoeft dus niet meer van niks te beginnen, dit scheelt veel tijd."},
-        {"text":"Begonnen met het gebruiken van javascript op een hogere niveau. Importeren van library's en API's aanroepen. De concepten van OOP beginnen door te dringen."},
-        {"text":"Tijdens het volgen van lessen over datavisualisatie leerde ik d3.js gebruiken. Hiermee kwam de concepten van het bouwen van visualisaties naar voren. Denk aan enter(), update() en exit()."},
-        {"text":"Veel geleerd in deze periode over hoe handig GIT cli kan zijn. En de eerste stappen met SASS gemaakt. Denk hierbij aan het toepassen van SMACSS en BEM methodes voor gestructureerde en herbruikbare CSS."},
-        {"text":"Naarmate ik steeds grotere projecten ging doen, werd een betere stuctuur steeds  meer essentieel, daarom ben ik begonnen met een framework genaamd Express.js."},
-    ];
+    var itemEntry = {   
+        entry: [
+            {
+                "title": "Datavisualisatie",
+                "paragraph": "visualisatie van coding activiteit",
+                "img": "./assets/images/datavis-logo.png",
+                "mod-block": "odd"
+            },
+            {
+                "title": "Resizer",
+                "paragraph": "Pas de dimensies van je afbeelding aan",
+                "img": "./assets/images/og_grrr.png",
+                "mod-block": "even bla"
+            },
+            {
+                "title": "Melkweg",
+                "paragraph": "SASS aanpassingen in de melkweg",
+                "img": "./assets/images/melkweg.png",
+                "mod-block": "odd blo"
+            },
+            {
+                "title": "Score app",
+                "paragraph": "Toevoegen van actuele score's",
+                "img": "./assets/images/score-app-logo.png",
+                "mod-block": "even fourth"
+            },
+            {
+                "title": "Klassiekwijzer",
+                "paragraph": "Kom erachter welke klassieke muziek bij je past",
+                "img": "./assets/images/klassiekwijzer.png",
+                "mod-block": "odd fifth"
+            },
+            {
+                "title": "Pathogen",
+                "paragraph": "RTS game gemaakt met javascript",
+                "img": "./assets/images/pathogen_logo.png",
+                "mod-block": "even sixth"
+            },
+        ]
+    };
+    var textEntry = {
+        visie: [
+            {
+                "title": "Werkwijze",
+                "text":"Als Frontend Developer is het mijn taak om ervoor te zorgen dat interactie tussen de gebruiker en digitale interactieve producten vloeiend en gebruiksvriendelijk gebeurt.",
+                "img": "../assets/images/modularv1.png",
+                "mod-description":"right",
+                "mod-container":"left"
+            },   
+            {
+                "title": "Modulair",
+                "text":"Tijdens het programeren splits ik mijn code afhankelijk van zijn functies op in aparte modules, zodat het achteraf makkelijk te onderhouden is",
+                "img": "../assets/images/modularv4.png",
+                "mod-description":"reverse",
+                "mod-container":"reverse"
+            },
+            {
+                "title": "Herbruikbaar",
+                "text":"Naast dat aparte modules een duidelijk overzicht maakt van de geschreven code, is het ook eenvoudig om het in een andere project toe te voegen. Dit maakt de code een stuk beter herbruikbaar. Je hoeft dus niet meer van niks te beginnen, dit scheelt veel tijd.",
+                "img": "../assets/images/modularv2.png",
+                "mod-description":"right",
+                "mod-container":"left"
+            },
+        ],
+        tijdlijn: [
+            {
+                "title": "Haboes",
+                "link":"http://www.haboes.nl",
+                "mod-period": "--left",
+                "mod-item":"--left",
+                "mod-description": "--left",
+                "sub-title": "Javascript",
+                "text":"Begonnen met het gebruiken van javascript op een hogere niveau. Importeren van library's en API's aanroepen. De concepten van OOP beginnen door te dringen."
+            },
+            {
+                "title": "Datavisualisatie",
+                "link":"http://d3js.org",
+                "mod-period": "",
+                "mod-item":"",
+                "mod-description": "",
+                "sub-title": "D3",
+                "text":"Tijdens het volgen van lessen over datavisualisatie leerde ik d3.js gebruiken. Hiermee kwam de concepten van het bouwen van visualisaties naar voren. Denk aan enter(), update() en exit()."
+            },
+            {
+                "title": "Grrr",
+                "link":"http://www.grrr.nl",
+                "mod-period": "--left",
+                "mod-item":"--left",
+                "mod-description": "--left",
+                "sub-title": "GIT & SASS",
+                "text":"Veel geleerd in deze periode over hoe handig GIT cli kan zijn. En de eerste stappen met SASS gemaakt. Denk hierbij aan het toepassen van SMACSS en BEM methodes voor gestructureerde en herbruikbare CSS."
+            },
+            {
+                "title": "afstuderen",
+                "link":"http://expressjs.com/",
+                "mod-period": "--active",
+                "mod-item":"",
+                "mod-description": "",
+                "sub-title": "Express",
+                "text":"Naarmate ik steeds grotere projecten ging doen, werd een betere stuctuur steeds  meer essentieel, daarom ben ik begonnen met een framework genaamd Express.js."
+            },
+        ]
+    };
     var name = [
         {'name': "GIT", 'score': 10},
         {'name': "Javascript", 'score': 18},
@@ -151,10 +222,8 @@ module.exports.create = function(request, response){
     ];
     //if true start rendering
     response.render('index', {  
-        img: imgEntry,
         text: textEntry,
-        title: titleEntry,
-        paragraph: paragraphEntry,
+        item: itemEntry,
         datavisual: true,
         normal: true,
         nav_1: true,
