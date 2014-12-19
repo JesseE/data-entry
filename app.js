8a383145d55c2d57188b7e83cff739c4d0e3a13c
jazzcoder <jesse.eikema1991@gmail.com>
2014-12-19 18:55:37 +0100

    mongoclient removed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var port = 3000;
 var exphbs = require('express3-handlebars');
 
 // var mongoose = require('mongoose');
-mongoClient.connect('mongodb://localhost:27017', function(err, db) {
+// mongoClient.connect('mongodb://localhost:27017', function(err, db) {
     // mongoose.connect('mongodb://localhost:27017', function(err, db) {
     // if(err){throw err;}
     
@@ -66,7 +66,7 @@ mongoClient.connect('mongodb://localhost:27017', function(err, db) {
 //     collection.find().toArray(function(err, results){
 //         console.log(results); 
 //     });   
-});
+// });
 
 //passport implementation
 // var passport = require('passport');
